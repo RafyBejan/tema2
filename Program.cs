3f9460e7bcedad9f9d563cd9a732e5b2582e7540
RafyBejan <146414646+RafyBejan@users.noreply.github.com>
2023-10-12 22:49:32 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x;
+            x = Convert.ToInt32(Console.ReadLine());
+            
+            if(x%2==0)
+            {
+                Console.WriteLine("Par");
+            }
+            else
+            {
+                Console.WriteLine("Impar");
+            }
+
+            
+        
+           
+        }
+    }
+}
